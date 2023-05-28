a1a31c6a26a9c21b9acc6ca1f15283edf62ca866
Andrei Beliavtsev <buggisart@gmail.com>
2023-05-28 23:43:44 +0400

    test

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Mesto1Test {
 
@@ -17,15 +18,13 @@ public class Mesto1Test {
     }
 
     @Test
-    @DisplayName("Add a new photo")
-    @Description("This test is for adding a new photo to Mesto.")
-    public void addNewPhoto() {
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
         given()
-                .header("Content-type", "application/json") // Передаём Content-type в заголовке для указания типа файла
                 .auth().oauth2(bearerToken) // Передаём токен для аутентификации
-                .body("{\"name\":\"Москва\",\"link\":\"https://code.s3.yandex.net/qa-automation-engineer/java/files/paid-track/sprint1/photoSelenium.jpg\"}") // Формируем тело запроса
-                .post("/api/cards") // Делаем POST-запрос
-                .then().statusCode(201); // Проверяем код ответа
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
     }
 
     @Test
